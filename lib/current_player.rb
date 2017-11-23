88a6307995638a1abec99634240cdb10d684ed3b
Kriti Rai <kriti.rai@gmail.com>
2017-11-23 05:14:48 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,9 @@
 def turn_count(board)
+  counter = 0
   board.each do |position|
-    counter = 0
+    
     if position == "X" || "O"
       return counter += 1
-  end
+    end
 end
 # def current_player(board)
